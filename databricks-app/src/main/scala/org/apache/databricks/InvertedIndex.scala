a812e69d59d7e4c68eca71d44bc78bc502a631d1
sam ma <sam_newyork@163.com>
2021-08-25 01:20:32 +0800

    use spark rdd api to transform data for records aggregate

--- databricks-app/src/main/scala/org/apache/databricks/InvertedIndex.scala
+++ databricks-app/src/main/scala/org/apache/databricks/InvertedIndex.scala
@@ -1,6 +1,7 @@
 package org.apache.databricks
 
 import com.typesafe.scalalogging.Logger
+import org.apache.commons.configuration.{PropertiesConfiguration => HierConf}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -12,13 +13,32 @@ object InvertedIndex {
 
   private[this] val logger = Logger(InvertedIndex.getClass)
 
+  /*
+   * => 数据文件内容:
+   * 0  it is what it is
+   * 1  what is it
+   * 2  it is a banana
+   */
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("invertedIndex")
       .set("spark.serializer", "org.apache.spark.serializer.JavaSerializer")
       .set("spark.ui.port", "4701")
     val sparkContext = new SparkContext(conf)
+    logger.info("command line args {} when start spark app", args)
 
-//    val cfg = new HierConf()
+    val cfg = new HierConf(args(0))
+    val inputFile = cfg.getString("inputfile")
+    sparkContext.textFile(inputFile).map(line => line.split("\t"))
+      .map(phases => (phases(0), phases(1)))
+      // 将(fname, "it is what it is")元组map为 [("it", fname), ("is", fname), ("what", fname), ("is", fname)]
+      .map(tuple => tuple._2.split(" ").map(word => (word, tuple._1)))
+      // 将RDD[Array[(String, String)]]通过flatmap拍平为RDD[("it", fname)]
+      .flatMap(element => element)
+      //
+      .reduceByKey((fname, file) => {
+
+      })
+    sparkContext.stop()
   }
 
 }

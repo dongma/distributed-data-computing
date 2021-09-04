4620cf50a39b73d9039f86a8f37e905673090689
sam ma <sam_newyork@163.com>
2021-09-04 10:15:59 +0800

    complete inverted index with words frequency on spark 3.1.2

--- databricks-app/src/main/scala/org/apache/databricks/InvertedIndex.scala
+++ databricks-app/src/main/scala/org/apache/databricks/InvertedIndex.scala
@@ -1,8 +1,7 @@
 package org.apache.databricks
 
 import com.typesafe.scalalogging.Logger
-import org.apache.commons.configuration.{PropertiesConfiguration => HierConf}
-import org.apache.spark.rdd.RDD
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -15,35 +14,47 @@ object InvertedIndex {
   private[this] val logger = Logger(InvertedIndex.getClass)
 
   /*
-   * => 数据文件内容:
-   * 0  it is what it is
-   * 1  what is it
-   * 2  it is a banana
+   * => 数据文件内容 (文件列表有：0、1、2):
+   * 0. it is what it is
+   * 1. what is it
+   * 2. it is a banana
    */
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("invertedIndex")
-      .set("spark.serializer", "org.apache.spark.serializer.JavaSerializer")
-      .set("spark.ui.port", "4701")
-    val sparkContext = new SparkContext(conf)
-    logger.info("command line args {} when start spark app", args)
+      .setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("WARN")
 
-    val cfg = new HierConf(args(0))
-    val inputFile = cfg.getString("inputfile")
-    val wordAggregate: RDD[(String, Map[String, Int])] = sparkContext.textFile(inputFile).map(line => line.split("\t"))
-      .map(phases => (phases(0), phases(1)))
-      // 将(fname, "it is what it is")元组映射为[("it", (fname->1)), ("is", (fname->1)), ("what", (fname->1)), ("is", (fname->1))]
-      .map(tuple => tuple._2.split(" ").map(word => (word, Map(tuple._1 -> 1))))
-      // RDD[Array[(String, String)]]用flatmap拍平为RDD[("it", (fname->1))]
-      .flatMap(element => element)
-      // 针对相同reduce key，若在map中存在相同key，则累加key对应的value值
-      .reduceByKey((map1, map2) => {
-        val mergeMap = map1 ++ map2.map {
-          case(k, v) => k -> (map1.getOrElse(k, 0) + map2.getOrElse(k, 0))
-        }
-        mergeMap
-      })
-    logger.info("use rdd api to aggregate words, result: {}", wordAggregate)
-    sparkContext.stop()
+    val fs = FileSystem.get(sc.hadoopConfiguration)
+    val fileList = fs.listFiles(new Path("/tmp/input"), true)
+
+    // wordRdd中数据：[(0, "it"), (1, "what"), (2, "banana")]
+    var wordRdd = sc.emptyRDD[(String, String)]
+    while (fileList.hasNext) {
+      val path = fileList.next
+      wordRdd = wordRdd.union(sc.textFile(path.getPath.toString)
+        .flatMap(_.split("\\s+"))
+        .map((path.getPath.getName, _)))
+    }
+
+    // 按单词、文件进行累加，[((0, "it"), 2), ((0, "is"), 2), ((0, "what"), 1)]
+    val countRdd = wordRdd.map((_, 1)).reduceByKey(_ + _)
+    val invertedRdd = countRdd.map(data => (data._1._2, String.format("(%s, %s)", data._1._1, data._2.toString)))
+      // [("it", (0, 2)), ("is", (0, 2)), ("is", (2, 1))] => [("it", (0, 2)), ("is", (0, 2),(2, 1))]
+      .reduceByKey(_ + "," + _)
+      // [("is", (0, 2),(2, 1))] => [("is"), {(0, 2),(2, 1)}]
+      .map(tuple => String.format("\"%s\": {%s}", tuple._1, tuple._2))
+
+    /* 21/09/04 10:04:16 INFO BlockManagerMaster: Registered BlockManager BlockManagerId(driver, 192.168.1.13, 52312, None)
+    21/09/04 10:04:16 INFO BlockManager: Initialized BlockManager: BlockManagerId(driver, 192.168.1.13, 52312, None)
+    "what": {(1, 1),(0, 1)}
+    "banana": {(2, 1)}
+    "it": {(1, 1),(0, 2),(2, 1)}
+    "is": {(2, 1),(0, 2),(1, 1)}
+    "a": {(2, 1)}
+     */
+    invertedRdd.foreach(println)
+    sc.stop()
   }
 
 }

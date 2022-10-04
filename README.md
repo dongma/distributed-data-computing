0973a3dc06fd6298d22d0a1e8ee984c37d88cbb5
sam ma <sam_newyork@163.com>
2022-10-04 22:25:38 +0800

    use graphframes execute iterator computing on spark platform

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# 大数据的三驾马车之HDFS
+# 基于Spark和Neo4j进行图数据分析
 
-> `Hadoop`是`Apache`的一个开源分布式计算平台，核心是以`HDFS`分布式文件系统和`MapReduce`分布式计算框架构成，为用户提供了一套底层透明的分布式基础设施。其中`HDFS`提供了海量数据的存储，`MapReduce`提供了对数据的计算。
+> 基础环境是基于`Hadoop`搭建的，其是`Apache`的一个开源分布式计算平台，核心是以`HDFS`分布式文件系统和`MapReduce`分布式计算框架构成，为用户提供了一套底层透明的分布式基础设施。其中`HDFS`提供了海量数据的存储，`MapReduce`提供了对数据的计算。
 
 使用`docker`在本地快速构建`hadoop`服务，镜像名称为`sequenceiq/hadoop-docker:latest`
 
 ```bash
 > docker run -p 50070:50070 -p 9000:9000 -p 8088:8088 -it sequenceiq/hadoop-docker /etc/bootstrap.sh -bash
 ```
-
+部署`Neo4j`图数据库，可以通过`docker`部署或直接从官网下载`community`社区版本。建议下载社区版，其对`apoc`和`graph algorithm`插件的支持比较好。
+```bash
+> docker run -d --name neo4j_db -p 7474:7474 -p 7687:7687 -v /tmp/neo4j/data:/data -v /tmp/neo4j/logs:/logs -v /tmp/neo4j/conf:/var/lib/neo4j/conf -v /tmp/neo4j/import:/var/lib/neo4j/import -v /tmp/neo4j/plugins:/plugins --env NEO4J_AUTH=neo4j/password neo4j
+```
+图计算平台使用`apache-spark 3.0`，使用`pyspark`并基于`graphframe-0.8`组件进行图计算。基础数据点、边表基于`csv`文件，放在代码仓库`dataset`的文件夹下，基础数据导入`Neo4j`后，数据展示如下：
+<img src="dataset/doc_reference/neo4j_transport_data.jpg" alt="neo4j_transport_data" style="zoom:50%;" />
---
+++ pyspark_graph/path_finding/path_finding_algo.py
@@ -0,0 +1,117 @@
+# -*- coding:utf-8 -*-
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession, DataFrame
+
+from graphframes import GraphFrame
+from graphframes.lib import AggregateMessages as AM
+from pyspark.sql import functions as F
+
+
+def create_transport_graph(spark: SparkSession) -> GraphFrame:
+    """加载nodes和边relationships的csv，创建transport graph，用union是不分区边的direct"""
+    node_fields = [
+        StructField("id", StringType(), True),
+        StructField("latitude", FloatType(), True),
+        StructField("longitude", FloatType(), True),
+        StructField("population", IntegerType(), True)
+    ]
+    nodes = spark.read.csv("dataset/transport-nodes.csv", header=True,
+                           schema=StructType(node_fields))
+    # 读取加载交通关系relationships.csv内容，并构成graphframe
+    rels = spark.read.csv("dataset/transport-relationships.csv", header=True)
+    reversed_rels = (rels.withColumn("newSrc", rels.dst).withColumn("newDst", rels.src) \
+                     .drop("dst", "src").withColumnRenamed("newSrc", "src") \
+                     .withColumnRenamed("newDst", "dst") \
+                     .select("src", "dst", "relationship", "cost"))
+
+    relationships = rels.union(reversed_rels)
+    return GraphFrame(nodes, relationships)
+
+
+def getSparkContext() -> SparkSession:
+    builder = SparkSession.builder.appName("pandas-on-spark")\
+        .master("local[*]")
+    builder = builder.config("spark.sql.execution.arrow.pyspark.enabled", "true")
+    # Pandas API on Spark automatically uses this Spark session with the configurations set.
+    return builder.getOrCreate()
+
+
+add_path_udf = F.udf(lambda path, id: path + [id], ArrayType(StringType()))
+
+
+def shortest_path(g: GraphFrame, origin, destination, spark: SparkSession,
+                  column_name="cost") -> DataFrame:
+    """shortest_path用于从源节点开始计算最短路径，访问到目标后，就立即返回."""
+    sc = spark.sparkContext
+    if g.vertices.filter(g.vertices.id == destination).count() == 0:
+        return (spark.createDataFrame(sc.emptyRDD(), g.vertices.schema)
+                .withColumn("path", F.array()))
+
+    # 对vertices节点添加{"visited", "distance", "path"}属性，默认visited为false，当访问过节点后，visited为true
+    vertices = (g.vertices.withColumn("visited", F.lit(False))
+                .withColumn("distance", F.when(g.vertices["id"] == origin, 0).otherwise(float("inf")))
+                .withColumn("path", F.array()))
+    cached_vertices = AM.getCachedDataFrame(vertices)
+    g2 = GraphFrame(cached_vertices, g.edges)
+
+    # 当graph中所有节点都已被访问时，结束BSP发消息，结束计算模型
+    while g2.vertices.filter('visited == False').first():
+        current_node_id = g2.vertices.filter('visited == False')\
+            .sort("distance").first().id
+
+        msg_distance = AM.edge[column_name] + AM.src['distance']
+        msg_path = add_path_udf(AM.src["path"], AM.src["id"])
+        # 组装要发给dst的消息，消息结构包括struct(msg_distance, msg_path)内容，F.min(AM.msg)会过滤取distance最小的消息
+        msg_for_dst = F.when(AM.src['id'] == current_node_id, F.struct(msg_distance, msg_path))
+        new_distances = g2.aggregateMessages(F.min(AM.msg).alias('aggMess'),
+                                             sendToDst=msg_for_dst)
+
+        new_visited_col = F.when(g2.vertices.visited | (g2.vertices.id == current_node_id), True)\
+            .otherwise(False)
+        new_distance_col = F.when(new_distances["aggMess"].isNotNull() &
+                                  (new_distances.aggMess["col1"] < g2.vertices.distance), new_distances.aggMess["col1"])\
+            .otherwise(g2.vertices.distance)
+        new_path_col = F.when(new_distances["aggMess"].isNotNull() &
+                              (new_distances.aggMess["col1"] < g2.vertices.distance), new_distances.aggMess['col2'].cast("array<string>"))\
+            .otherwise(g2.vertices.path)
+        new_vertices = (g2.vertices.join(new_distances, on="id", how="left_outer").drop(new_distances["id"])
+                        .withColumn("visited", new_visited_col)
+                        .withColumn("newDistance", new_distance_col)
+                        .withColumn("newPath", new_path_col).drop("aggMess", "distance", "path")
+                        .withColumnRenamed("newDistance", "distance").withColumnRenamed("newPath", 'path'))
+        cached_new_vertices = AM.getCachedDataFrame(new_vertices)
+
+        g2 = GraphFrame(cached_new_vertices, g2.edges)
+        if g2.vertices.filter(g2.vertices.id == destination).first().visited:
+            return (g2.vertices.filter(g2.vertices.id == destination)
+                    .withColumn("newPath", add_path_udf("path", "id"))
+                    .drop("visited", "path")
+                    .withColumnRenamed("newPath", "path"))
+    return (spark.createDataFrame(sc.emptyRDD(), g.vertices.schema)
+            .withColumn("path", F.array()))
+
+
+if __name__ == '__main__':
+    spark: SparkSession = getSparkContext()
+    graph = create_transport_graph(spark)
+    # 先从所有点vertex中找到人口介于10万～30万的城市，并用show()方法展示出来
+    graph.vertices.filter("population > 100000 and population < 300000") \
+        .sort("population") \
+        .show()
+    # 从Den Haag到一个中型城市的最短路径，result.columns展示的是结果集中的列
+    from_expr: str = "id='Den Haag'"
+    to_expr: str = "population > 100000 and population < 300000 and id <> 'Den Haag'"
+    result = graph.bfs(from_expr, to_expr)
+    print(result.columns)
+    # 以e开头的列代表关系(边)，而以v开头的列代表节点(顶点)，我们仅对节点感兴趣
+    columns = [column for column in result.columns if not column.startswith("e")]
+    result.select(columns).show()
+
+    # +----------+--------+------------------------------------------------------------------------+
+    # | id | distance | path |
+    # +----------+--------+------------------------------------------------------------------------+
+    # | Colchester | 347.0 | [Amsterdam, Den Haag, Hoek van Holland, Felixstowe, Ipswich, Colchester] |
+    # +----------+--------+------------------------------------------------------------------------+
+    # 计算两个地点间的最短路径，"Amsterdam"和"Colchester"之间的距离，并打印出距两地间的距离
+    result = shortest_path(graph, "Amsterdam", "Colchester", spark, "cost")
+    result.select("id", "distance", "path").show(truncate=False)
